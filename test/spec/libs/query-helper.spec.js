7616b715a2be3eba080d374120544e2a38e6d17a
dyep49 <dyep49@gmail.com>
2015-01-26 02:43:01 -0500

    adds query helper tests

---
+++ test/spec/libs/query-helper.spec.js
@@ -0,0 +1,35 @@
+var config = require('../../../config/config.js');
+var queryHelper = require('../../../libs/query-helper.js');
+
+describe('the queryHelper module', function() {
+
+  it('exports a factory', function() {
+    var empty = Object.keys(queryHelper).length === 0 ? true : false;
+    empty.should.equal(false);
+  });
+
+  describe('#uniqueByProp', function() {
+
+    it('returns an array of unique objects by a prop given an array of objects', function() {
+
+      var data = [
+        {a: 1, b: 2},
+        {a: 3, b: 4},
+        {a: 1, b: 2},
+        {a: 6, b: 3}
+      ];
+
+      var unique = [
+        {a: 1, b: 2},
+        {a: 3, b: 4},
+        {a: 6, b: 3}
+      ]
+
+      var uniqueData = queryHelper.uniqueByProp(data, 'a');
+
+      JSON.stringify(uniqueData).should.equal(JSON.stringify(unique));
+    });
+  });
+
+
+})
